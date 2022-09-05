2bad13ef75e2f17f8332446f1ecda817226b37e9
Madi <madiasunik@gmail.com>
2022-09-06 00:22:27 +0600

    Module 4.1 Task #3 - second commit

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            System.out.println(28 + (month + month / 8) % 2 + 2 % month + 1 / month * 2);
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
